acc34ee117ef2da2a204048a4a13eb7b3c8bb128
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-26 06:09:31 +0000

    Taskモデルの作成、

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200725145938) do
+ActiveRecord::Schema.define(version: 20200726054737) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
