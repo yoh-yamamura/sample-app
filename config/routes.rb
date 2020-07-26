f39a43cc576bb2519d983971aa9f522de74aa9e9
yoh-yamamura <love.is.enemy928@gmail.com>
2020-07-26 12:11:37 +0000

    c3.c4実装。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tasks/new'
+
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users
+  resources :users do
+    resources :tasks
+  end
 end
