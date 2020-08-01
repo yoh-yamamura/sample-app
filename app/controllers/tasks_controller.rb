13a79b9c976c4ca24d590f8c24cf5068f5717dc1
yoh-yamamura <love.is.enemy928@gmail.com>
2020-08-01 07:27:49 +0000

    obj.create_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,22 @@
 class TasksController < ApplicationController
+  
+  def index
+     @user = User.find(params[:user_id])
+     @tasks = @user.tasks
+  end
+  
   def new
+    @task = Task.new
+  end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
