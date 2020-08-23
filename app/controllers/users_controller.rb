3a5e98581e512b440dc195970c7c9a8c08de56d3
yoh-yamamura <love.is.enemy928@gmail.com>
2020-08-23 22:00:53 +0000

    タスク開発D完了。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(show edit update destroy)
+  # before_action :logged_in_user, only: %i(index show edit update destroy)
+  # before_action :admin_user, only: %i(index destroy)
+  # before_action :correct_user, only: %i(edit update)
+  # before_action :admin_or_correct, only: %i(show)
+  
+  def index
+    # @user = User.find(params[:id])
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
   
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def new
+    if logged_in? && !current_user.admin?
+      flash[:info] = 'すでにログインしています。'
+      redirect_to current_user
+    end
     @user = User.new
   end
   
@@ -20,9 +34,11 @@ class UsersController < ApplicationController
   end
   
   def edit
+    # @user = User.find(params[:id])
   end
   
   def update
+    # @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -41,5 +57,9 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
 end
